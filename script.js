9ff737a1a28bde1ed924145fc5676451448989e9
Aarjan Ghimire <aarjanghimire.28@gmail.com>
2024-09-04 12:59:51 +0545

    some nostalgic SVG

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-circleTime(20);
+circleTime(30);
 
 function circleTime(second) {
     const strSec = second.toString();
     document.getElementById("circle").style.setProperty("animation-duration", strSec+"s");  
 }
+
+const pathElement = document.getElementById('circle');
+const pathLength = pathElement.getTotalLength();
+console.log(`Path length: ${pathLength}`);
